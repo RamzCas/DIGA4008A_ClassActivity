0be6bca6b4ebbe6c21a7594e00972ebd921600e2
RamzCas <2749414@students.wits.ac.za>
2026-02-24 10:34:11 +0200

    projectiles

--- DIGA4008A_ClassActvities/Assets/Scripts/P1 script.cs
+++ DIGA4008A_ClassActvities/Assets/Scripts/P1 script.cs
@@ -1,19 +1,55 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class P1script : MonoBehaviour
 {
-    [SerializeField] private float MoveSpeed = 5;
-    [SerializeField] private float JumpFource;
-    [SerializeField] private float RoatationSpeed;
-    private Vector2 lookInput;
-    private Vector2 moveInput;
+    [Header("Movement")]
+    [SerializeField] private float Movespeed = 5f;
+    [SerializeField] private float Jumpstep = 1f;
+    [SerializeField] private float Rotation = 100f;
 
+
+    [Header("Shooting")]
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Transform FirePoint;
+    [SerializeField] private float projectileSpeed = 12;
+    private Vector2 lookinput;
+    private Vector2 moveinput;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        float y = lookInput.x * RoatationSpeed * Time.deltaTime;
-        transform.Rotate(0f, y, 0f, Space.World);
+        float yaw = lookinput.x * Rotation * Time.deltaTime;
+        transform.Rotate(0f, yaw, 0f, Space.World);
 
-        Vector3 move3 = new Vector3(moveInput.x, 0f, moveInput.y) * MoveSpeed * Time.deltaTime;
+        Vector3 move3 = new Vector3(moveinput.x, 0f, moveinput.y) * Movespeed * Time.deltaTime;
+        transform.position += move3;
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        moveinput = context.ReadValue<Vector2>();
         
     }
+
+    public void Onlook(InputAction.CallbackContext context)
+    {
+        lookinput = context.ReadValue<Vector2>();
+    }
+
+    public void onjump(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        transform.position += Vector3.up * Jumpstep;
+    }
 }
+
+    
+
